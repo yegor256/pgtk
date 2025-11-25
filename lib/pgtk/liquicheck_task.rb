297751ceb6d39442db7cb74af9751c63cf14d171
Artem Yegorov <yegorov0725@yandex.ru>
2025-11-25 11:57:49 +0300

    #233: implement rake task for check Liquibase XML files

---
+++ lib/pgtk/liquicheck_task.rb
@@ -0,0 +1,139 @@
+# frozen_string_literal: true
+
+# SPDX-FileCopyrightText: Copyright (c) 2019-2025 Yegor Bugayenko
+# SPDX-License-Identifier: MIT
+
+require 'nokogiri'
+require 'rake/tasklib'
+require_relative '../pgtk'
+
+# Liquicheck rake task for check Liquibase XML files.
+# Author:: Yegor Bugayenko (yegor256@gmail.com)
+# Copyright:: Copyright (c) 2019-2025 Yegor Bugayenko
+# License:: MIT
+class Pgtk::LiquicheckTask < Rake::TaskLib
+  # Task name
+  # @return [Symbol]
+  attr_accessor :name
+
+  # Base directory where Liquibase XML files will be stored
+  # @return [String]
+  attr_accessor :dir
+
+  # Migration XML files pattern
+  # @return [String]
+  attr_accessor :pattern
+
+  def initialize(*args, &task_block)
+    super()
+    @name = args.shift || :liquicheck
+    @dir = 'liquibase'
+    @pattern = '*/*.xml'
+    @errors = {}
+    desc 'Check the quality of Liquibase XML files' unless ::Rake.application.last_description
+    task(name, *args) do |_, task_args|
+      RakeFileUtils.send(:verbose, true) do
+        yield(*[self, task_args].slice(0, task_block.arity)) if block_given?
+        run
+      end
+    end
+  end
+
+  private
+
+  def run
+    raise "Option 'dir' is mandatory" if !@dir || @dir.empty?
+    raise "Option 'pattern' is mandatory" if !@pattern || @pattern.empty?
+    Dir[File.join(File.expand_path(File.join(Dir.pwd, @dir)), @pattern)].each do |f|
+      with_file(f) do
+        doc = Nokogiri::XML(File.open(f))
+        doc.remove_namespaces!
+        path = doc.at_xpath('databaseChangeLog')&.attr('logicalFilePath')&.to_s
+        filled!(path, 'logicalFilePath')
+        doc.xpath('databaseChangeLog/changeSet').each do |node|
+          id = node.attr('id')&.to_s
+          author = node.attr('author')&.to_s
+          context = node.attr('context')&.to_s
+          filled!(id, 'id')
+          filled!(author, 'author')
+          present(author) do
+            no_match(author, /\A[-_ A-Za-z0-9]+\z/) do
+              error!("author '#{author}' has illegal symbols#{' in test context' if context == 'test'}")
+            end
+          end
+          if context == 'test'
+            present(id) do
+              no_match(id, /[-a-z]+/) do
+                error!("id '#{id}' has not suffix in test context")
+              end
+            end
+            present(id, path) do
+              match(id, /[-a-z]+/) do
+                no_match(path.gsub(/[-_.a-z]/, ''), /\A#{id.gsub(/[-a-z]/, '')}\z/) do
+                  error!("id '#{id}' is not the beginning of a logicalFilePath '#{path}' in test context")
+                end
+              end
+            end
+          else
+            present(id, path) do
+              no_match(path.gsub(/[-_.a-z]/, ''), /\A#{id}\z/) do
+                error!("id '#{id}' is not the beginning of a logicalFilePath '#{path}'")
+              end
+            end
+          end
+        end
+        present(path) do
+          no_equal(path, File.basename(f)) do
+            error!("logicalFilePath '#{path}' does not match the xml file name '#{File.basename(f)}'")
+          end
+        end
+      end
+    end
+    return if @errors.empty?
+    puts 'There are such errors in the Liquibase XML files.'
+    @errors.each do |f, e|
+      puts "In file '#{f}':"
+      e.each do |msg|
+        puts "  * #{msg}"
+      end
+      puts
+    end
+    exit(1)
+  end
+
+  def with_file(file, &)
+    @f = file
+    yield
+  ensure
+    @f = nil
+  end
+
+  def error!(msg)
+    return if @f.nil?
+    (@errors[@f] ||= []) << msg
+  end
+
+  def filled!(prop, name)
+    if prop.nil?
+      error!("#{name} is nil")
+    elsif prop.empty?
+      error!("#{name} is empty")
+    end
+  end
+
+  def present(*props, &)
+    yield if props.all? { !_1.nil? && !_1.empty? }
+  end
+
+  def no_equal(prop, val, &)
+    yield if prop != val
+  end
+
+  def match(prop, regex, &)
+    yield if prop.match?(regex)
+  end
+
+  def no_match(prop, regex, &)
+    yield unless prop.match?(regex)
+  end
+end
---
+++ test/test_liquicheck_task.rb
@@ -0,0 +1,297 @@
+# frozen_string_literal: true
+
+# SPDX-FileCopyrightText: Copyright (c) 2019-2025 Yegor Bugayenko
+# SPDX-License-Identifier: MIT
+
+require 'tmpdir'
+require 'nokogiri'
+require 'qbash'
+require_relative 'test__helper'
+require_relative '../lib/pgtk/liquicheck_task'
+
+# Liquicheck rake task test.
+# Author:: Yegor Bugayenko (yegor256@gmail.com)
+# Copyright:: Copyright (c) 2017-2025 Yegor Bugayenko
+# License:: MIT
+class TestLiquicheckTask < Pgtk::Test
+  def test_simple
+    Dir.mktmpdir do |dir|
+      File.write(
+        File.join(dir, '001-some-migration.xml'),
+        migration(
+          logical_file_path: '001-some-migration.xml',
+          id: '001',
+          author: 'yegor256'
+        )
+      )
+      File.write(
+        File.join(dir, '002-some-migration.xml'),
+        migration(
+          logical_file_path: '002-some-migration.xml',
+          id: '',
+          author: 'user'
+        )
+      )
+      File.write(
+        File.join(dir, '003-migration.xml'),
+        migration(
+          logical_file_path: '003-some-migration.xml',
+          id: '123',
+          author: '*'
+        )
+      )
+      File.write(
+        File.join(dir, '004-some-migration.xml'),
+        migration(
+          logical_file_path: '004-migration.xml',
+          id: '400',
+          author: '*',
+          context: 'test'
+        )
+      )
+      File.write(
+        File.join(dir, '005-some-migration.xml'),
+        migration(
+          logical_file_path: '005-migration.xml',
+          id: '006-test',
+          author: 'user',
+          context: 'test'
+        )
+      )
+      Pgtk::LiquicheckTask.new(:liquicheck) do |t|
+        t.dir = '.'
+        t.pattern = '*.xml'
+      end
+      assert_raises(SystemExit) do
+        assert_output(/There are such errors in the Liquibase XML files/) do
+          Dir.stub(:pwd, dir) do
+            Rake::Task['liquicheck'].invoke
+          end
+        end
+      end
+    end
+  end
+
+  def test_without_xml
+    Dir.mktmpdir do |dir|
+      File.join(File.expand_path(__dir__), '..', 'lib', 'pgtk', 'liquicheck_task').then do |r|
+        File.write(File.join(dir, 'Rakefile'), <<~RUBY)
+          require '#{r}'
+
+          Pgtk::LiquicheckTask.new
+        RUBY
+      end
+      assert_empty(qbash("cd #{Shellwords.escape(dir)} && bundle exec rake liquicheck"))
+    end
+  end
+
+  def test_without_errors
+    Dir.mktmpdir do |dir|
+      File.write(File.join(dir, 'Rakefile'), rakefile)
+      File.write(
+        File.join(dir, '001-some-migration.xml'),
+        migration(
+          logical_file_path: '001-some-migration.xml',
+          id: '001',
+          author: 'yegor256'
+        )
+      )
+      File.write(
+        File.join(dir, '002-other-migration.xml'),
+        migration(
+          logical_file_path: '002-other-migration.xml',
+          id: '002',
+          author: 'yegor256'
+        )
+      )
+      File.write(
+        File.join(dir, '003-some-migration.xml'),
+        migration(
+          logical_file_path: '003-some-migration.xml',
+          id: '003-test',
+          author: 'yegor256',
+          context: 'test'
+        )
+      )
+      assert_empty(qbash("cd #{Shellwords.escape(dir)} && bundle exec rake liquicheck"))
+    end
+  end
+
+  def test_with_empty_attributes
+    Dir.mktmpdir do |dir|
+      File.write(File.join(dir, 'Rakefile'), rakefile)
+      File.write(
+        File.join(dir, '001-some-migration.xml'),
+        migration(
+          logical_file_path: '',
+          id: '001',
+          author: 'user'
+        )
+      )
+      File.write(
+        File.join(dir, '002-some-migration.xml'),
+        migration(
+          logical_file_path: '002-some-migration.xml',
+          id: '',
+          author: 'user'
+        )
+      )
+      File.write(
+        File.join(dir, '003-some-migration.xml'),
+        migration(
+          logical_file_path: '003-some-migration.xml',
+          id: '003',
+          author: ''
+        )
+      )
+      File.write(
+        File.join(dir, '004-some-migration.xml'),
+        migration(
+          logical_file_path: '',
+          id: '004-test',
+          author: 'user',
+          context: 'test'
+        )
+      )
+      File.write(
+        File.join(dir, '005-some-migration.xml'),
+        migration(
+          logical_file_path: '005-some-migration.xml',
+          id: '',
+          author: 'user',
+          context: 'test'
+        )
+      )
+      File.write(
+        File.join(dir, '006-some-migration.xml'),
+        migration(
+          logical_file_path: '006-some-migration.xml',
+          id: '006-test',
+          author: '',
+          context: 'test'
+        )
+      )
+      out, e = qbash("cd #{Shellwords.escape(dir)} && bundle exec rake liquicheck", accept: nil, both: true)
+      refute_empty(out)
+      assert_equal(1, e)
+      assert_match(/001-some-migration.xml.+\n.+logicalFilePath is empty/, out)
+      assert_match(/002-some-migration.xml.+\n.+id is empty/, out)
+      assert_match(/003-some-migration.xml.+\n.+author is empty/, out)
+      assert_match(/004-some-migration.xml.+\n.+logicalFilePath is empty/, out)
+      assert_match(/005-some-migration.xml.+\n.+id is empty/, out)
+      assert_match(/006-some-migration.xml.+\n.+author is empty/, out)
+    end
+  end
+
+  def test_with_incorrect_attributes
+    Dir.mktmpdir do |dir|
+      File.write(File.join(dir, 'Rakefile'), rakefile)
+      File.write(
+        File.join(dir, '001-some-migration.xml'),
+        migration(
+          logical_file_path: '001-some-migration.xml',
+          id: '001',
+          author: '!*'
+        )
+      )
+      File.write(
+        File.join(dir, '002-some-migration.xml'),
+        migration(
+          logical_file_path: '002-some-migration.xml',
+          id: '002-test',
+          author: '#@',
+          context: 'test'
+        )
+      )
+      File.write(
+        File.join(dir, '003-some-migration.xml'),
+        migration(
+          logical_file_path: '003-some-migration.xml',
+          id: '00',
+          author: 'user'
+        )
+      )
+      File.write(
+        File.join(dir, '004-some-migration.xml'),
+        migration(
+          logical_file_path: '004-some-migration.xml',
+          id: '00-test',
+          author: 'user'
+        )
+      )
+      File.write(
+        File.join(dir, '005-some-migration.xml'),
+        migration(
+          logical_file_path: '005-some-migration.xml',
+          id: '005',
+          author: 'user',
+          context: 'test'
+        )
+      )
+      File.write(
+        File.join(dir, '006-some-migration.xml'),
+        migration(
+          logical_file_path: '006-other-migration.xml',
+          id: '006',
+          author: 'user'
+        )
+      )
+      out, e = qbash("cd #{Shellwords.escape(dir)} && bundle exec rake liquicheck", accept: nil, both: true)
+      refute_empty(out)
+      assert_equal(1, e)
+      assert_match(/001-some-migration.xml.+\n.+author '!\*' has illegal symbols/, out)
+      assert_match(/002-some-migration.xml.+\n.+author '#@' has illegal symbols in test context/, out)
+      assert_match(
+        /003-some-migration.xml.+\n.+id '00' is not the beginning of a logicalFilePath '003-some-migration.xml'/,
+        out
+      )
+      assert_match(
+        /004-some-migration.xml.+\n.+id '00-test' is not the beginning of a logicalFilePath '004-some-migration.xml'/,
+        out
+      )
+      assert_match(/005-some-migration.xml.+\n.+id '005' has not suffix in test context/, out)
+      assert_match(
+        /006-some-migration.xml.+\n.+logicalFilePath\s
+        '006-other-migration.xml'\sdoes\snot\smatch\sthe\sxml\sfile\sname\s'006-some-migration.xml'/x,
+        out
+      )
+    end
+  end
+
+  private
+
+  def rakefile
+    File.join(File.expand_path(__dir__), '..', 'lib', 'pgtk', 'liquicheck_task').then do |r|
+      <<~RUBY
+        require '#{r}'
+
+        Pgtk::LiquicheckTask.new do |t|
+          t.dir = '.'
+          t.pattern = '*.xml'
+        end
+      RUBY
+    end
+  end
+
+  def migration(logical_file_path: '', id: '', author: '', context: nil, sql: 'CREATE TABLE t (f TEXT);')
+    Nokogiri::XML::Builder.new(encoding: 'UTF-8') do |xml|
+      xml.comment(<<~TXT)
+        * SPDX-FileCopyrightText: Copyright (c) 2025 Yegor Bugayenko
+        * SPDX-License-Identifier: MIT
+      TXT
+      xml.databaseChangeLog(
+        'xmlns' => 'http://www.liquibase.org/xml/ns/dbchangelog',
+        'xmlns:xsi' => 'http://www.w3.org/2001/XMLSchema-instance',
+        'xsi:schemaLocation' => 'http://www.liquibase.org/xml/ns/dbchangelog' \
+                                'http://www.liquibase.org/xml/ns/dbchangelog/dbchangelog-2.0.xsd',
+        'logicalFilePath' => logical_file_path
+      ) do
+        xml.changeSet({ 'id' => id, 'author' => author, 'context' => context }.compact) do
+          xml.sql do
+            xml.text(sql)
+          end
+        end
+      end
+    end.to_xml
+  end
+end
